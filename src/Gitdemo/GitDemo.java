49a3e3caf96f901b96854c59c1c23362eae1243c
One <One@SONY>
2018-05-15 05:07:38 -0700

    first demo of git

---
+++ src/Gitdemo/GitDemo.java
@@ -0,0 +1,13 @@
+package Gitdemo;
+
+public class GitDemo {
+
+	public static void main(String[] args) 
+	
+	
+	{
+	System.out.println("git sucessfully working");
+
+	}
+
+}
---
+++ src/Gitdemo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author One
+ *
+ */
+package Gitdemo;
